46d1e90f0bd35183fd5c2969b35187fdc2321ada
Ajay Mahajan <116831587+Ajay-mahajan-19@users.noreply.github.com>
2023-10-03 17:58:14 +0530

    Add files via upload

---
+++ onclick.js
@@ -0,0 +1,24 @@
+function red(){
+    let a;
+   a=document.getElementById("a").value;
+   document.getElementById("color").style.background=a; 
+    }
+    
+    function green(){
+      let b;
+     b=document.getElementById("b").value;
+     document.getElementById("color").style.background=b;          
+    } 
+    
+   function blue(){
+     let c;
+     c=document.getElementById("c").value;
+     document.getElementById("color").style.background=c;
+   }
+   
+   function magenta(){
+     let d;
+     d=document.getElementById("d").value;
+     document.getElementById("color").style.background=d;
+   }
+    
